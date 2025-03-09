c505ea79fc8552c75c51e33ca94f566ce435565d
pradana argo pangestu <60965994+Pradana123@users.noreply.github.com>
2025-03-09 23:53:00 +0700

    Create namapraktik.cs

---
+++ 02_Pengenalan_IDE_dan_Pemprograman_CSharp/Modul2/namapraktik.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Meminta input nama
+        Console.Write("Masukkan nama Anda: ");
+        string nama = Console.ReadLine();
+        Console.WriteLine($"Selamat datang, {nama}!");
+    }
+}
