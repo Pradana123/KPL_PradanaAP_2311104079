a4317b87a76afae4f5a406cc0c31d0600024bc9f
pradana argo pangestu <60965994+Pradana123@users.noreply.github.com>
2025-04-10 23:20:45 +0700

    Create HaloGeneric.cs

---
+++ 05_Generics/TPMODUL5_2311104079/HaloGeneric.cs
@@ -0,0 +1,67 @@
+using System;
+
+class HaloGeneric
+{
+    public void SapaUser<T>(T user)
+    {
+        Console.WriteLine($"Halo user {user}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        HaloGeneric halo = new HaloGeneric();
+ HEAD
+        halo.SapaUser("dan");
+    }
+}
+
+class DataGeneric<T>
+{
+    private T data;
+
+    public DataGeneric(T data)
+    {
+        this.data = data;
+    }
+
+    public void PrintData()
+    {
+        Console.WriteLine($"Data yang tersimpan adalah: {data}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        DataGeneric<string> data = new DataGeneric<string>("2311104079"); 
+        data.PrintData();
+    }
+}
+
+class DataGeneric<T>
+{
+    private T data;
+
+    public DataGeneric(T data)
+    {
+        this.data = data;
+    }
+
+    public void PrintData()
+    {
+        Console.WriteLine($"Data yang tersimpan adalah: {data}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        DataGeneric<string> data = new DataGeneric<string>("2311104079"); 
+        data.PrintData();
+    }
+}
