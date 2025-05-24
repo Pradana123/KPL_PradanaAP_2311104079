5c58c24ff1b1bad78a66741fb9da6c60877956a4
pradana argo pangestu <60965994+Pradana123@users.noreply.github.com>
2025-05-24 20:34:24 +0700

    Add files via upload

---
+++ 09_API_Design_dan_Construction_Using_Swagger/JurnalModul9_2311104079/Controllers/MoviesController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using JurnalModul9_2311104079.Models;
+using System.Collections.Generic;
+
+namespace JurnalModul9_2311104079.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MoviesController : ControllerBase
+    {
+        private static List<Movie> Movies = new List<Movie>
+        {
+            new Movie
+            {
+                Title = "The Shawshank Redemption",
+                Director = "Frank Darabont",
+                Stars = new List<string> { "Tim Robbins", "Morgan Freeman" },
+                Description = "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency."
+            },
+            new Movie
+            {
+                Title = "The Godfather",
+                Director = "Francis Ford Coppola",
+                Stars = new List<string> { "Marlon Brando", "Al Pacino" },
+                Description = "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son."
+            },
+            new Movie
+            {
+                Title = "The Dark Knight",
+                Director = "Christopher Nolan",
+                Stars = new List<string> { "Christian Bale", "Heath Ledger" },
+                Description = "When the menace known as the Joker emerges, Batman must accept one of the greatest psychological and physical tests."
+            }
+        };
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Movie>> GetMovies()
+        {
+            return Ok(Movies);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Movie> GetMovie(int id)
+        {
+            if (id < 0 || id >= Movies.Count)
+            {
+                return NotFound();
+            }
+            return Ok(Movies[id]);
+        }
+
+        [HttpPost]
+        public ActionResult<Movie> AddMovie([FromBody] Movie movie)
+        {
+            Movies.Add(movie);
+            return CreatedAtAction(nameof(GetMovie), new { id = Movies.Count - 1 }, movie);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteMovie(int id)
+        {
+            if (id < 0 || id >= Movies.Count)
+            {
+                return NotFound();
+            }
+            Movies.RemoveAt(id);
+            return NoContent();
+        }
+    }
+}
---
+++ 09_API_Design_dan_Construction_Using_Swagger/JurnalModul9_2311104079/Models/Movie.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace JurnalModul9_2311104079.Models
+{
+    public class Movie
+    {
+        public string Title { get; set; }
+        public string Director { get; set; }
+        public List<string> Stars { get; set; }
+        public string Description { get; set; }
+
+    }
+}
