cff02ebb89eabad6750a6d0183ca335259237db6
pradana argo pangestu <60965994+Pradana123@users.noreply.github.com>
2025-05-24 19:44:44 +0700

    Add files via upload

---
+++ 09_API_Design_dan_Construction_Using_Swagger/TP/TPModul9_2311104079/Controllers/MahasiswaController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TPModul9_2311104079.Models;
+
+namespace TPModul9_2311104079.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MahasiswaController : ControllerBase
+    {
+        private static List<Mahasiswa> daftarMahasiswa = new List<Mahasiswa>()
+        {
+            new Mahasiswa { Nama = "Pradana Argo Pangestu", NIM = "2311104079" },
+            new Mahasiswa { Nama = "Jauhar Fajar", NIM = "2311104055" },
+            new Mahasiswa { Nama = "Rizaldy AUlia", NIM = "2311104056" },
+
+        };
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Mahasiswa>> Get()
+        {
+            return daftarMahasiswa;
+        }
+
+        [HttpGet("{index}")]
+        public ActionResult<Mahasiswa> Get(int index)
+        {
+            if (index < 0 || index >= daftarMahasiswa.Count)
+                return NotFound("Mahasiswa tidak ditemukan");
+            return daftarMahasiswa[index];
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Mahasiswa mhs)
+        {
+            daftarMahasiswa.Add(mhs);
+            return Ok("Mahasiswa berhasil ditambahkan");
+        }
+
+        [HttpDelete("{index}")]
+        public IActionResult Delete(int index)
+        {
+            if (index < 0 || index >= daftarMahasiswa.Count)
+                return NotFound("Index tidak valid");
+            daftarMahasiswa.RemoveAt(index);
+            return Ok("Mahasiswa berhasil dihapus");
+        }
+    }
+}
---
+++ 09_API_Design_dan_Construction_Using_Swagger/TP/TPModul9_2311104079/Models/Mahasiswa.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TPModul9_2311104079.Models;
+
+namespace TPModul9_2311104079.Models
+{
+    public class Mahasiswa
+    {
+        public string Nama { get; set; }
+        public string NIM { get; set; }
+    }
+}
+
