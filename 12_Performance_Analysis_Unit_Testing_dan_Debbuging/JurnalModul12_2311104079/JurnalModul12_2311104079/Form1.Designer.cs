72c7b28abc893e107299ef7a26bacadfac4e6dc0
pradana argo pangestu <60965994+Pradana123@users.noreply.github.com>
2025-05-25 13:57:59 +0700

    Add files via upload

---
+++ 12_Performance_Analysis_Unit_Testing_dan_Debbuging/JurnalModul12_2311104079/JurnalModul12_2311104079/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace JurnalModul12_2311104079
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtBase = new System.Windows.Forms.TextBox();
+            this.txtExponent = new System.Windows.Forms.TextBox();
+            this.btnHitung = new System.Windows.Forms.Button();
+            this.lbloutput = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtBase
+            // 
+            this.txtBase.Location = new System.Drawing.Point(184, 112);
+            this.txtBase.Name = "txtBase";
+            this.txtBase.Size = new System.Drawing.Size(100, 20);
+            this.txtBase.TabIndex = 0;
+            this.txtBase.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtBase.TextChanged += new System.EventHandler(this.txtBase_TextChanged);
+            // 
+            // txtExponent
+            // 
+            this.txtExponent.Location = new System.Drawing.Point(460, 112);
+            this.txtExponent.Name = "txtExponent";
+            this.txtExponent.Size = new System.Drawing.Size(100, 20);
+            this.txtExponent.TabIndex = 1;
+            this.txtExponent.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtExponent.TextChanged += new System.EventHandler(this.txtExponent_TextChanged);
+            // 
+            // btnHitung
+            // 
+            this.btnHitung.Location = new System.Drawing.Point(595, 332);
+            this.btnHitung.Name = "btnHitung";
+            this.btnHitung.Size = new System.Drawing.Size(75, 23);
+            this.btnHitung.TabIndex = 2;
+            this.btnHitung.Text = "Hitung";
+            this.btnHitung.UseVisualStyleBackColor = true;
+            this.btnHitung.Click += new System.EventHandler(this.btnHitung_Click);
+            // 
+            // lbloutput
+            // 
+            this.lbloutput.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lbloutput.Location = new System.Drawing.Point(248, 156);
+            this.lbloutput.Name = "lbloutput";
+            this.lbloutput.Size = new System.Drawing.Size(229, 147);
+            this.lbloutput.TabIndex = 3;
+            this.lbloutput.Text = "menampilkan hasil output";
+            this.lbloutput.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lbloutput.Click += new System.EventHandler(this.lbloutput_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbloutput);
+            this.Controls.Add(this.btnHitung);
+            this.Controls.Add(this.txtExponent);
+            this.Controls.Add(this.txtBase);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtBase;
+        private System.Windows.Forms.TextBox txtExponent;
+        private System.Windows.Forms.Button btnHitung;
+        private System.Windows.Forms.Label lbloutput;
+    }
+}
+
---
+++ 12_Performance_Analysis_Unit_Testing_dan_Debbuging/JurnalModul12_2311104079/JurnalModul12_2311104079/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JurnalModul12_2311104079
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void lbloutput_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtExponent_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtBase_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        public static int CariNilaiPangkat(int a, int b)
+        {
+            if (b == 0) return 1;
+            if (b < 0) return -1;
+
+            try
+            {
+                checked
+                {
+                    int hasil = 1;
+                    for (int i = 0; i < b; i++)
+                    {
+                        hasil *= a;
+                    }
+                    if (b > 10 || a > 100) return -2;
+                    return hasil;
+                }
+            }
+            catch (OverflowException)
+            {
+                return -3;
+            }
+        }
+        private void btnHitung_Click(object sender, EventArgs e)
+        {
+            int a, b;
+
+            bool isAValid = int.TryParse(txtBase.Text, out a);
+            bool isBValid = int.TryParse(txtExponent.Text, out b);
+
+            if (!isAValid || !isBValid)
+            {
+                lbloutput.Text = "Input tidak valid!";
+                return;
+            }
+
+            int hasil = CariNilaiPangkat(a, b);
+            lbloutput.Text = $"Hasil: {hasil}";
+        }
+    }
+}
