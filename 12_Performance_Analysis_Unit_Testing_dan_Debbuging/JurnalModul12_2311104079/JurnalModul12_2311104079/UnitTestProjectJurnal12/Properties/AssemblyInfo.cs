d950553eb964cbebeb889622d31064c9c6a3e92d
pradana argo pangestu <60965994+Pradana123@users.noreply.github.com>
2025-05-25 13:58:52 +0700

    Add files via upload

---
+++ 12_Performance_Analysis_Unit_Testing_dan_Debbuging/JurnalModul12_2311104079/JurnalModul12_2311104079/UnitTestProjectJurnal12/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProjectJurnal12")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProjectJurnal12")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("fd285574-3bba-4aa7-b023-f01dbc53c020")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ 12_Performance_Analysis_Unit_Testing_dan_Debbuging/JurnalModul12_2311104079/JurnalModul12_2311104079/UnitTestProjectJurnal12/Properties/UnitTest1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using JurnalModul12_2311104079;
+
+namespace UnitTestProjectJurnal12.Properties
+{
+
+[TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestPangkatNormal()
+        {
+            Assert.AreEqual(8, Form1.CariNilaiPangkat(2, 3));
+        }
+
+        [TestMethod]
+        public void TestPangkat0()
+        {
+            Assert.AreEqual(1, Form1.CariNilaiPangkat(0, 0));
+        }
+
+        [TestMethod]
+        public void TestPangkatNegatif()
+        {
+            Assert.AreEqual(-1, Form1.CariNilaiPangkat(3, -1));
+        }
+
+        [TestMethod]
+        public void TestBPangkatLebihDari10()
+        {
+            Assert.AreEqual(-2, Form1.CariNilaiPangkat(2, 11));
+        }
+
+        [TestMethod]
+        public void TestANilaiLebihDari100()
+        {
+            Assert.AreEqual(-2, Form1.CariNilaiPangkat(101, 2));
+        }
+
+        [TestMethod]
+        public void TestOverflow()
+        {
+            Assert.AreEqual(-3, Form1.CariNilaiPangkat(99999, 10));
+        }
+    }
+
+}
