98515dac1e160c2b24745485634c4ebe04298505
pradana argo pangestu <60965994+Pradana123@users.noreply.github.com>
2025-05-25 12:39:57 +0700

    Add files via upload

---
+++ 10_Library_Consruction/JurnalModul10_2311104079/AppPemanggil/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using MatematikaLibraries;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("FPB(60, 45) = " + Matematika.FPB(60, 45));
+        Console.WriteLine("KPK(12, 8) = " + Matematika.KPK(12, 8));
+        Console.WriteLine("Turunan({1, 4, -12, 9}) = " + Matematika.Turunan(new int[] { 1, 4, -12, 9 }));
+        Console.WriteLine("Integral({4, 6, -12, 9}) = " + Matematika.Integral(new int[] { 4, 6, -12, 9 }));
+    }
+}
---
+++ 10_Library_Consruction/JurnalModul10_2311104079/MatematikaLibraries/Matematika.cs
@@ -0,0 +1,50 @@
+﻿namespace MatematikaLibraries
+{
+    public class Matematika
+    {
+        public static int FPB (int a, int b)
+        {
+            while (b != 0)
+            {
+                int tempt = b;
+                b = a % b;
+                a = tempt;
+            }
+            return a;
+        }
+        public static int KPK (int a, int b)
+        {
+            return (a * b) / FPB(a, b);
+        }
+        public static string Turunan(int[]koef)
+        {
+            List<string> hasil = new();
+            for (int i = 0; i < koef.Length - 1; i++)
+            {
+                int pangkat = koef.Length - 1 - i;
+                int baruKoef = koef[i] * pangkat;
+                if (baruKoef == 0) continue;
+                string suku = $"{baruKoef}x";
+                if (pangkat - 1 > 1) suku += $"^{pangkat - 1}";
+                else if (pangkat - 1 == 1) suku += "";
+                hasil.Add(suku);
+            }
+            return string.Join(" + ", hasil).Replace("+ -", "- ");
+        }
+        public static string Integral(int[] koef)
+        {
+            List<string> hasil = new();
+            for (int i = 0; i < koef.Length; i++)
+            {
+                int pangkat = koef.Length - i - 1 + 1 ;
+                double baruKoef = (double)koef[i] / pangkat;
+                string suku = $"{baruKoef}x";
+                if (pangkat > 1) 
+                    suku += $"^{pangkat}";
+                hasil.Add(suku);
+            }
+            hasil.Add("C");
+            return string.Join("+", hasil).Replace("+ -", "- ");
+        }
+    }
+}
