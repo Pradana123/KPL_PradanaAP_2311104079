8fe57d57f1176f2dcfb59eef880ff05136c559fe
pradana argo pangestu <60965994+Pradana123@users.noreply.github.com>
2025-06-05 18:52:15 +0700

    Add files via upload

---
+++ 14_Clean_Code/JurnalModul14_2311104079/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using PusatDataSingleton;
+
+class Program
+{
+    static void Main()
+    {
+        var pusat = PusatData.GetInstance();
+
+        pusat.TambahkanData("Joko");
+        pusat.TambahkanData("Bambang");
+        pusat.TambahkanData("Rian");
+
+        Console.WriteLine("Data awal:");
+        pusat.PrintSemuaData();
+
+        pusat.HapusSebuahData(2); // hapus "Rian"
+
+        Console.WriteLine("\nData setelah penghapusan:");
+        pusat.PrintSemuaData();
+
+        Console.ReadKey();
+    }
+}
---
+++ 14_Clean_Code/JurnalModul14_2311104079/PusatDataSingleton.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PusatDataSingleton
+{
+    public class PusatData
+    {
+        private static PusatData? _instance;
+        private List<string> data = new();
+
+        // Constructor private agar tidak bisa diinstansiasi dari luar
+        private PusatData() { }
+
+        // Method untuk mendapatkan satu-satunya instance (Singleton)
+        public static PusatData GetInstance()
+        {
+            if (_instance == null)
+                _instance = new PusatData();
+            return _instance;
+        }
+
+        public void TambahkanData(string item)
+        {
+            data.Add(item);
+        }
+
+        public void HapusSebuahData(int index)
+        {
+            if (index >= 0 && index < data.Count)
+                data.RemoveAt(index);
+        }
+
+        public void PrintSemuaData()
+        {
+            Console.WriteLine("Isi Data:");
+            foreach (var item in data)
+                Console.WriteLine("- " + item);
+        }
+    }
+}
